7d267123275e3fb5a383912c50af87c8a71f08ef
tillnelown <hllpd200@gmail.com>
2025-12-19 00:20:10 +0100

    Update Requests.kt

--- app/src/main/java/me/ayra/ha/healthconnect/network/Requests.kt
+++ app/src/main/java/me/ayra/ha/healthconnect/network/Requests.kt
@@ -1,6 +1,7 @@
 package me.ayra.ha.healthconnect.network
 
 import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.lagradost.nicehttp.ResponseParser
